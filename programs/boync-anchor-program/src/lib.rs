ec0d61bfe42e85756a189b743d60947b9e5faaa6
Daniel Pavel <daniel.pavel19@gmail.com>
2022-07-14 12:45:38 +0300

    [BA:2] Add Boync User program and test

--- programs/boync-anchor-program/src/lib.rs
+++ programs/boync-anchor-program/src/lib.rs
@@ -1,15 +1,50 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("EkvnvRY2prU1sJpVLHBk5qsNMXBryaZeMVacRSE1pcZM");
+
+pub const BOYNC_USER_PDA_SEED: &[u8] = b"user";
 
 #[program]
 pub mod boync_anchor_program {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String) -> Result<()> {
+        if name.as_bytes().len() > 64 {
+            return err!(ErrorCode::UserNameTooLong);
+        }
+        
+        let user_data = &mut ctx.accounts.user_data;
+        user_data.name = name;
+        user_data.user = ctx.accounts.user.key();
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init, 
+        payer = user,
+        space = BOYNC_USER_DATA_LEN,
+        seeds = [BOYNC_USER_PDA_SEED.as_ref(), user.key().as_ref()], 
+        bump,
+    )]
+    pub user_data: Account<'info, BoyncUserData>,
+    pub system_program: Program<'info, System>,
+}
+
+pub const BOYNC_USER_DATA_LEN: usize = 32 + (64 + 4) + 8;
+#[account]
+pub struct BoyncUserData {
+    pub user: Pubkey,
+    pub name: String, 
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("User name can only be 64 chars long.")]
+    UserNameTooLong,
+}
--- tests/boync-anchor-program.ts
+++ tests/boync-anchor-program.ts
@@ -2,15 +2,38 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { BoyncAnchorProgram } from "../target/types/boync_anchor_program";
 
+import { PublicKey } from '@solana/web3.js';
+import { expect } from "chai";
+
 describe("boync-anchor-program", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.BoyncAnchorProgram as Program<BoyncAnchorProgram>;
 
-  it("Is initialized!", async () => {
+  it("Creating user Bob", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const [bobUserPDA, _] = await PublicKey
+      .findProgramAddress(
+        [
+          anchor.utils.bytes.utf8.encode('user'),
+          provider.wallet.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
+
+    const tx = await program.methods
+      .initialize('Bob')
+      .accounts({
+        user: provider.wallet.publicKey,
+        userData: bobUserPDA,
+      })
+      .rpc();
+
+    console.log("[Success] Your transaction signature", tx);
+
+    const bobData = await program.account.boyncUserData.fetch(bobUserPDA);
+    expect(bobData.name).to.be.eql('Bob');
   });
 });
